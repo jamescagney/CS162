d4b1a319825cdaec7027afab2ec311b398c094ed
James Edward Cagney <45402150+jamescagney@users.noreply.github.com>
2026-04-06 09:50:29 -0700

    You did good!

--- SpenserKing.py
+++ SpenserKing.py
@@ -1,3 +1,4 @@
+#Nice, Spenser....
 # Project requires TWO functions:
 # 1. rect_area (length, width) which will return the area of a rectangle
 # 2. rect_surface_area (length, width, height) which will return the area of a solid rectangular object
